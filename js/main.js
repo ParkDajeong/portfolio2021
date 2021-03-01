68d0e608d7b4de9d9d0e656b355ab80aadbdca62
ParkDajeong <sobeast980@gmail.com>
2021-03-01 17:34:28 +0900

    작업표시줄 레이아웃 구현 / 잠금화면 -> 배경화면 전환 이벤트 구현

--- js/main.js
+++ js/main.js
@@ -1,13 +1,45 @@
+function addZero(num) {
+  return (num < 10 ? `0${num}` : num);
+}
+
+function getTimeText(hour, min) {
+  const h = 12 - hour;
+  let str = "";
+
+  if(h < 0) {
+    str = `오후 ${-h}:${addZero(min)}`;
+  } else if(h > 0) {
+    str = `오전 ${h}:${addZero(min)}`;
+  } else {
+    str = `오후 12시:${addZero(min)}`;
+  }
+
+  return str;
+}
+
 $(function() {
   // Loading
   setTimeout(function() {
     $(".login__loading").removeClass("on");
     $(".login__account").addClass("on");
-  }, 5000);
+  }, 4000);
   
-  // 로그인 이벤트
+  // Login Event
   $(".account__btn-login").on("click", function() {
     $(".login__loading").addClass("on enter");
     $(".login__account").removeClass("on");
+    setTimeout(function() {
+      $(".login, .login__loading").removeClass("on");
+    }, 2300);
   });
+
+  // Clock
+  setInterval(function() {
+    const today = new Date();
+    const hour = today.getHours();
+    const min = today.getMinutes();
+    
+    $(".clock").text(getTimeText(hour, min));
+  }, 1000);
+
 });
